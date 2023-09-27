79f135f4d2c6b30cfe7dae6014a9187f571127c3
Mikhail Demin <velociraptor_m@bk.ru>
2023-09-27 19:31:50 +0600

    new commit in new branch

--- Practice.java
+++ Practice.java
@@ -2,6 +2,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("Hello, world! v2.0");
-        System.out.println("Create a new branch in IJ!");
+        System.out.println("New commit in new branch");
     }
 }
